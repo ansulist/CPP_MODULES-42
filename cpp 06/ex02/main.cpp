586166b2cc6eefdae58136043915dd77bbf11190
Anita Sulistiyawati <kunyit@Anitas-Mac-mini.local>
2024-07-26 21:29:45 +0400

    06

--- cpp 06/ex02/main.cpp
+++ cpp 06/ex02/main.cpp
@@ -59,36 +59,21 @@ void    identify(Base &test)
     } catch(const std::exception& e){}
 }
 
-int     main( void )
+int main()
 {
-    std::srand(std::time(0)); 
-    
-    Base*   a = generate();
-    Base*   b = generate();
-    Base*   c = generate();
-    Base*   d = generate();
-
-    std::cout << "/* **************************************** */" << std::endl;
-
-    std::cout << "a* = "; identify( a );
-    std::cout << "a& = "; identify( *a ); std::cout << std::endl;
-
-    std::cout << "b* = "; identify( b );
-    std::cout << "b& = "; identify( *b ); std::cout << std::endl;
-
-    std::cout << "c* = "; identify( c );
-    std::cout << "c& = "; identify( *c ); std::cout << std::endl;
-
-    std::cout << "d* = "; identify( d );
-    std::cout << "d& = "; identify( *d ); std::cout << std::endl;
-
-    std::cout << "/* ***************************************** */" << std::endl;
-
-
-    delete a;
-    delete b;
-    delete c;
-    delete d;
-
-    return (0);
+	std::srand(std::time(0));
+	for (int j = 0; j < 5; j++)
+	{
+		Base *Test = generate();
+		if (Test == NULL)
+			return (1);
+		else
+		{
+			identify(Test);
+			identify(*Test);
+			delete (Test);
+			std::cout << std::endl;
+		}
+	}
+	return (0);
 }
