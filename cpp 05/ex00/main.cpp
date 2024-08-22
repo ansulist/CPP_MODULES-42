5ea32a5b7d876722b8e41d13bb26d431a7d093c9
Anita Sulistiyawati <ansulist@lab2r4s10.42abudhabi.ae>
2024-08-22 15:00:40 +0400

    try

--- cpp 05/ex00/main.cpp
+++ cpp 05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ansulist <ansulist@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:06:48 by ansulist          #+#    #+#             */
-/*   Updated: 2024/08/22 14:23:33 by ansulist         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:59:39 by ansulist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int main (void)
     try
     {
         Bureaucrat a ("anita", 1);
-        std::cout << "bghth" << a << std::endl;
+        std::cout << "bghth " << a << std::endl;
         Bureaucrat b;
         Bureaucrat c ("anita", 2);
-        a.incrementGrade();
-		std::cout << b << std::endl;
+        //a.incrementGrade();
+		std::cout << c << std::endl;
     } 
     catch (Bureaucrat::GradeTooHighException &example)
     {
@@ -30,10 +30,14 @@ int main (void)
 
     try
     {
-        Bureaucrat a ("anita", 150);
+        Bureaucrat a ("anita", 140);
         Bureaucrat b;
-        Bureaucrat c ("anita", 155);
+        //Bureaucrat c ("anita", 130);
         a.decrementGrade();
+		//std::cout << c << std::endl;
+		Bureaucrat c(b);
+		std::cout << c << std::endl;
+		// std::cout << b << std::endl;
     } 
     catch (Bureaucrat::GradeTooLowException &example)
     {
