52fcd517dc9237dc2f5fd28be92cb1c578e85383
Anita Sulistiyawati <kunyit@Anitas-Mac-mini.local>
2024-08-31 21:04:32 +0400

    main fixed

--- cpp 07/ex02/main.cpp
+++ cpp 07/ex02/main.cpp
@@ -17,7 +17,27 @@ int main ( void )
 	Array<int> b(3);
 	Array<int> c = b;
 	Array<int> *test = new Array<int>(5);
+	Array<char> z(5);
 
+	// char array
+	for (int i = 0; i < 5; i++)
+	{
+		z[i] = 'h';
+	}
+
+	printA(z);
+	std::cout << std::endl << "-------------------" << std::endl;
+	try
+	{
+		z[10] = 'a';
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+		std::cout << std::endl << "-------------------" << std::endl;
+	}
+
+	// int array
 	printA(a);
 	std::cout << std::endl << "-------------------" << std::endl;
 	b[2] = 10;
@@ -49,6 +69,7 @@ int main ( void )
 	std::cout << "arr size: "<< a.size() << std::endl;
 	std::cout << "arr size: "<< b.size() << std::endl;
 	std::cout << "arr size: "<< c.size() << std::endl;
+	std::cout << "arr size: "<< z.size() << std::endl;
 	delete(test);
 	return (0);
 
--- cpp 08/ex02/main.cpp
+++ cpp 08/ex02/main.cpp
@@ -3,20 +3,30 @@
 
 int main()
 {
-    MutantStack<char> anita_stack;
+    // MutantStack<char> anita_stack;
 
-    // Add characters from 'a' to 'z' to the stack
-    for (char ch = 'a'; ch <= 'z'; ++ch) {
-        anita_stack.push(ch);
-    }
-    std::cout << anita_stack.size() << std::endl;
-    std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
-    // Print the stack (note: printing is done in reverse order for stack)
-    while (!anita_stack.empty())
-    {
-        std::cout << anita_stack.top() << std::endl;
-        anita_stack.pop();
-    }
+    // // Add characters from 'a' to 'z' to the stack
+    // for (char ch = 'a'; ch <= 'z'; ++ch) {
+    //     anita_stack.push(ch);
+    // }
+    // MutantStack<char>::iterator ite = anita_stack.begin();
+    // MutantStack<char>::iterator itee = anita_stack.end();
+    // ++ite;
+    // --ite;
+    // while (ite != itee)
+    // {
+    //     std::cout << *ite << std::endl;
+    //     ++ite;
+    // }
+    // std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
+    // std::cout << anita_stack.size() << std::endl;
+    // std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
+    // // Print the stack (note: printing is done in reverse order for stack)
+    // while (!anita_stack.empty())
+    // {
+    //     std::cout << anita_stack.top() << std::endl;
+    //     anita_stack.pop();
+    // }
 
 
     std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
@@ -29,20 +39,20 @@ int main()
     mstack.pop();
     std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
     std::cout << mstack.size() << std::endl;
+    std::cout << "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    std::stack<int> s(mstack);
+    // MutantStack<int>::iterator it = mstack.begin();
+    // MutantStack<int>::iterator ite = mstack.end();
+    // ++it;
+    // --it;
+    // while (it != ite)
+    // {
+    //     std::cout << *it << std::endl;
+    //     ++it;
+    // }
+    // std::stack<int> s(mstack);
     return 0;
 }
