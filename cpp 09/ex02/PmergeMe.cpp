f67762b7758f922ce24a110d97120a923c9e739e
Anita Sulistiyawati <kunyit@Anitas-Mac-mini.local>
2024-09-05 08:50:23 +0400

    09999

--- cpp 09/ex02/PmergeMe.cpp
+++ cpp 09/ex02/PmergeMe.cpp
@@ -24,3 +24,38 @@ PmergeMe::PmergeMe(const PmergeMe &other)
 	*this = other;
 }
 
+int PmergeMe::check_nbr(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        if (!isdigit(str[i]))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int PmergeMe::atoii(const char* str)
+{
+    int result = 0;
+    int sign = 1;
+    const char* ptr = str;
+
+    while (*ptr == ' ' || *ptr == '\t')
+        ++ptr;
+    if (*ptr == '-') {
+        sign = -1;
+        ++ptr;
+    }
+    else if (*ptr == '+') {
+        ++ptr;
+    }
+    while (*ptr >= '0' && *ptr <= '9')
+	{
+        result = result * 10 + (*ptr - '0');
+        ++ptr;
+    }
+    return result * sign;
+}
+
--- cpp 09/ex02/PmergeMe.hpp
+++ cpp 09/ex02/PmergeMe.hpp
@@ -11,10 +11,15 @@ class PmergeMe
 		std::vector<int> vec;
 		std::list<int> lstt;
 	public:
+		// Orthodox 
 		PmergeMe();
 		~PmergeMe();
 		PmergeMe(const PmergeMe &other);
 		PmergeMe &operator=(const PmergeMe &copy);
+
+		// ----- algorithm for sorting
+		int check_nbr(char *str);
+		int atoii(const char* str);
 };
 
 #endif
